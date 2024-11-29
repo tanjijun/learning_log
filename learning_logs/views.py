91fdca3e6a8e2bc6ea97e4b4dd28f7806fabcc02
baini <1485890854@qq.com>
2024-11-29 15:23:35 +0800

    web

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
+
+# 在这里创建视图
+def index(request):
+    """学习笔记的主页"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """显示所有主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """显示单个主题及其所有的条目"""
+    topics = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topics': topics, 'entries': entries}
+    return render(request, 'learning_logs/topics.html', context)
+
